6770ab4f4e7609744243af3abcd48f2f3b52f3d7
Oseas Santana <oseas.santana@adventhealth.com>
2021-01-23 16:36:04 -0300

    Updates

--- precache-manifest.3fb8c67a946727038133145548ea8f3d.js
+++ precache-manifest.3fb8c67a946727038133145548ea8f3d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63e9dc1eabf79dd212d291af70187d66",
+    "revision": "1d4361240b7f61a7d479409bcf1070e9",
     "url": "/mentoria-kyrius-aulas/index.html"
   },
   {
-    "revision": "356b94400c35757ac61f",
+    "revision": "b2462cb745ee039ba67a",
     "url": "/mentoria-kyrius-aulas/static/css/main.2ff737a0.chunk.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mentoria-kyrius-aulas/static/js/2.47e74ccd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "356b94400c35757ac61f",
-    "url": "/mentoria-kyrius-aulas/static/js/main.b00ebc93.chunk.js"
+    "revision": "b2462cb745ee039ba67a",
+    "url": "/mentoria-kyrius-aulas/static/js/main.789ebb73.chunk.js"
   },
   {
     "revision": "dc192b57bfed2afd6be1ea5558f17255",
-    "url": "/mentoria-kyrius-aulas/static/js/main.b00ebc93.chunk.js.LICENSE.txt"
+    "url": "/mentoria-kyrius-aulas/static/js/main.789ebb73.chunk.js.LICENSE.txt"
   },
   {
     "revision": "4ee6742ff18b63260df0",
