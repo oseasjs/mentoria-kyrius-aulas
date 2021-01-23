f4c899bc8a6330f108a3509f18fbbf838878560b
Oseas Santana <oseas.santana@adventhealth.com>
2021-01-23 16:04:01 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mentoria-kyrius-aulas/precache-manifest.2a77e605beae474a439ce2b0ae1e13c7.js"
+  "/mentoria-kyrius-aulas/precache-manifest.b4daeaaf0fb727a706240ede5d75c9ee.js"
 );
 
 self.addEventListener('message', (event) => {
